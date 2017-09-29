f31b258c96d788203e5ddf21b8e5c7f24b4abdaf
carlosarriagacm <carlosarriagacm@gmail.com>
2017-09-29 09:46:39 -0500

    hwY635: Replace vendor/cm with vendor/lineage

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,9 +26,9 @@ INITIAL_COPYRIGHT_YEAR=2015
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -36,10 +36,10 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true"
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" "true"
 
 # Copyright headers and guards
-write_headers "che10 cherry"
+write_headers "hwY635"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
@@ -63,7 +63,7 @@ write_footers
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 
     # Copyright headers and guards
     write_headers
