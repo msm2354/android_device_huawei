555a541f148be4dc0e44abcb672d86fb54830ae5
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-11-13 11:14:17 -0600

    Update and rename lineage.mk to xpe.mk

--- xpe.mk
+++ xpe.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/huawei/hwY635/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xperience/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := hwY635
-PRODUCT_NAME := lineage_hwY635
+PRODUCT_NAME := xpe_hwY635
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := hwY635
 PRODUCT_MANUFACTURER := HUAWEI
